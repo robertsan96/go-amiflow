725bcca5f683eda3dc5ad4527032830b08337f85
robertsan96 <sandrurobert@gmail.com>
2022-10-07 08:31:55 +0300

    clean: removed useless struct

--- router/router.go
+++ router/router.go
@@ -1,10 +1,5 @@
 package router
 
-import "github.com/gin-gonic/gin"
-
-type ginHandler struct {
-	GinContext gin.Context
-}
 type PersistentStorageGroupRouter[T any] interface {
 	ReadPersistentStorageGroup(p T)
 }
